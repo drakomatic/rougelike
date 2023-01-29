8d9abb656fc619ef02ef89c8ae1c39fd97db1b36
drakomatic <draketurmonphantom@gmail.com>
2023-01-29 16:21:09 -0700

    pew pew pew

--- Game.py
+++ Game.py
@@ -5,15 +5,16 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
-scrsize = [1000,1000]
+scrsize = [1600,900]
 screen = pygame.display.set_mode(scrsize)
 pygame.display.set_caption("Drakomatic")
 rooms = []
 cameraposition = [0,0]
 room_color = (105, 43, 49)
+allprojs = []
 
 def magnitude(vec):
-    return math.sqrt(vec[0]**2 + vec[1]**2)
+    return math.sqrt(abs(vec[0]**2 + vec[1]**2))
 
 def getedges(center,size):
     halfsize = numpy.divide(size,2)
@@ -52,11 +53,29 @@ def getinsideroom(pos,size):
     return (inside, closestblock, closestedge)
 
 #setup reusable classes
+class weapon:
+    def __init__(self, firerate, projectiles, bulletspeed, playerowned, damage):
+        self.firerate = firerate
+        self.projectilecount = projectiles
+        self.bulletspeed = bulletspeed
+        self.nextfiretick = 0
+        self.playerowned = playerowned
+        self.damage = damage
+    def update(self, deltatime):
+        global clock, allprojs, plr, scrsize
+        mbuttons = pygame.mouse.get_pressed(3)
+        if mbuttons[0] and self.nextfiretick <= (pygame.time.get_ticks()/1000):
+            self.nextfiretick = (pygame.time.get_ticks()/1000) + (60/self.firerate)
+            mousetoplayer = numpy.subtract(pygame.mouse.get_pos(),numpy.divide(scrsize,2))
+            computespeed = numpy.multiply(numpy.divide(mousetoplayer,magnitude(mousetoplayer)),self.bulletspeed)
+            allprojs.append(projectile(computespeed, plr.position, self.playerowned, self.damage))
+
 class player:
-    def __init__(self, color):
+    def __init__(self, color, weapon):
         self.position = [0,0]
         self.color = color
         self.speed = 320
+        self.wep = weapon
     def update(self, deltatime):
         pressed = pygame.key.get_pressed()
         leftright = (pressed[pygame.K_d] and 1 or 0) - (pressed[pygame.K_a] and 1 or 0)
@@ -64,13 +83,14 @@ class player:
         movevec = [leftright, updown]
         if magnitude(movevec) > 1:
             movevec = numpy.divide(movevec, magnitude(movevec))
+        self.wep.update(deltatime)
         #collision is amazing
         inroom, nearroom, nearedge = getinsideroom(self.position,12)
         self.position = numpy.add(self.position, numpy.multiply(numpy.multiply(movevec,self.speed), deltatime))
         if not inroom:
             xory = (nearedge % 2 == 0)
-            x = (xory and getedges(nearroom.position, nearroom.size)[nearedge] + (nearedge < 1 and -13 or 13) or self.position[0])
-            y = (xory and self.position[1] or getedges(nearroom.position, nearroom.size)[nearedge] + (nearedge > 1 and -13 or 13))
+            x = (xory and getedges(nearroom.position, nearroom.size)[nearedge] + (nearedge < 1 and -12.1 or 12.1) or self.position[0])
+            y = (xory and self.position[1] or getedges(nearroom.position, nearroom.size)[nearedge] + (nearedge > 1 and -12.1 or 12.1))
             self.position = [x,y]
 
 
@@ -80,6 +100,8 @@ class projectile:
         self.velocity = vel
         self.damage = damage
         self.isfromplayer = fromplr
+    def update(self, deltatime):
+        self.position = numpy.add(self.position, numpy.multiply(self.velocity, deltatime))
 
 class room: #the name room is for ease of access
     def __init__(self, position, size):
@@ -87,9 +109,8 @@ class room: #the name room is for ease of access
         self.size = size
 
 #some temp vars
-rooms.append(room([0,0], [800,800]))
-rooms.append(room([500,0], [400,200]))
-plr = player((252, 254, 248))
+rooms.append(room([0,0], [1200,1200]))
+plr = player((252, 254, 248),weapon(600,1,600,True,4))
 
 def lerp(a,b,f):
     return (a * (1-f)) + (b * f)
@@ -104,7 +125,7 @@ def convertposition(pos):
     return numpy.subtract(numpy.add(pos,numpy.divide(scrsize,2)), cameraposition)
 
 def renderall():
-    global rooms, plr, screen, room_color
+    global rooms, plr, screen, room_color, allprojs
     screen.fill((42, 24, 38))
 
     #render all rooms (order shouldn't matter because they are all the same color)
@@ -113,6 +134,9 @@ def renderall():
         newrect.center = convertposition(rom.position)
         pygame.draw.rect(screen, room_color, newrect)
     
+    for bullet in allprojs:
+        pygame.draw.circle(screen, (255,188,22), convertposition(bullet.position),4)
+
     #render player on top of everything
     pygame.draw.circle(screen, plr.color, convertposition(plr.position),12)
 
@@ -127,6 +151,8 @@ while running:
     deltatime = clock.tick(clock.get_fps())/1000
 
     plr.update(deltatime)
+    for i in allprojs:
+        i.update(deltatime)
 
     #update the camera pos towards the player pos
     centeredmousepos = numpy.subtract(pygame.mouse.get_pos(),numpy.divide(scrsize,2))
